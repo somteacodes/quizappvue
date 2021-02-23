3334835163b29f082d5ee3d39cb2429b4fd126c2
somteacodes <stomteacodes@gmail.com>
2021-02-24 00:08:49 +0100

    Load next question

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,7 @@
         <!-- score container -->
         <div class="text-right text-gray-800">
           <p class="text-sm leading-3">Score</p>
-          <p class="font-bold">60</p>
+          <p class="font-bold">{{ score }}</p>
         </div>
 
         <!-- timer container -->
@@ -64,7 +64,9 @@
         <!-- progress indicator container -->
         <div class="mt-8 text-center">
           <div class="h-1 w-12 bg-gray-800 rounded-full mx-auto"></div>
-          <p class="font-bold text-gray-800">2/10</p>
+          <p class="font-bold text-gray-800">
+            {{ questionCounter }}/{{ questions.length }}
+          </p>
         </div>
       </div>
     </div>
@@ -86,8 +88,10 @@ import { onMounted, ref } from "vue";
 export default {
   setup() {
     // data
+    let canClick = true;
 
     let questionCounter = ref(0);
+    let score = ref(0);
     const currentQuestion = ref({
       question: "",
       answer: 1,
@@ -118,8 +122,18 @@ export default {
       },
     ];
 
-    const onQuizStart = () => {
-      currentQuestion.value = questions[questionCounter.value];
+    const loadQuestion = () => {
+      canClick = true;
+      // Check if there are more questions to load
+      if (questions.length > questionCounter.value) {
+        // load question
+        currentQuestion.value = questions[questionCounter.value];
+        console.log("Current questions", currentQuestion.value);
+        questionCounter.value++;
+      } else {
+        // no more questions
+        console.log("Out of questions");
+      }
     };
 
     // methods/functions
@@ -130,31 +144,50 @@ export default {
       }
     };
 
+    const clearSelected = (divSelected) => {
+      setTimeout(() => {
+        divSelected.classList.remove("option-correct");
+        divSelected.classList.remove("option-wrong");
+        divSelected.classList.add("option-default");
+        loadQuestion();
+      }, 1000);
+    };
+
     const onOptionClicked = (choice, item) => {
-      const divContainer = itemsRef[item];
-      const optionID = item + 1;
-      if (currentQuestion.value.answer == optionID) {
-        console.log("you are correct");
-        divContainer.classList.add("option-correct");
-        divContainer.classList.remove("option-default");
+      if (canClick) {
+        const divContainer = itemsRef[item];
+        const optionID = item + 1;
+        if (currentQuestion.value.answer == optionID) {
+          console.log("you are correct");
+          score.value += 10;
+          divContainer.classList.add("option-correct");
+          divContainer.classList.remove("option-default");
+        } else {
+          console.log("you are wrong");
+          divContainer.classList.add("option-wrong");
+          divContainer.classList.remove("option-default");
+        }
+        canClick = false;
+        // TODO go to next question
+        clearSelected(divContainer);
+        console.log(choice, item);
       } else {
-        console.log("you are wrong");
-        divContainer.classList.add("option-wrong");
-        divContainer.classList.remove("option-default");
+        // Cant select option
+        console.log("cant select question");
       }
-      console.log(choice, item);
     };
 
     // lifecycle hooks
     onMounted(() => {
-      onQuizStart();
+      loadQuestion();
     });
     // return
     return {
       currentQuestion,
       questions,
+      score,
       questionCounter,
-      onQuizStart,
+      loadQuestion,
       onOptionClicked,
       optionChosen,
     };
